9e9257a52a00e3297321202556d76dd943697a5a
Kevin Martensson <kevinmartensson@gmail.com>
2014-04-23 14:21:21 +0200

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,23 @@
+/*global describe, it */
+'use strict';
+
+var assert = require('assert');
+var fs = require('fs');
+var Imagemin = require('image-min');
+var pngquant = require('../');
+var path = require('path');
+
+describe('pngquant()', function () {
+    it('should optimize a PNG', function (cb) {
+        var imagemin = new Imagemin();
+
+        imagemin
+            .src(path.join(__dirname, 'fixtures/test.png'))
+            .use(pngquant())
+            .optimize(function (err, file) {
+                assert(file.contents.length < fs.statSync(imagemin.src()).size);
+                assert(file.contents.length > 0);
+                cb();
+            });
+    });
+});
