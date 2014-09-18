1b6c4e54f3ada1b6135bae1b303b4668eff596c2
Brian Kennedy <bkennedy@rmn.com>
2014-09-18 11:38:03 -0500

    Don't error when the file size is too large, just skip

--- index.js
+++ index.js
@@ -13,6 +13,22 @@ var pngquant = require('pngquant-bin').path;
 
 module.exports = function (opts) {
 	opts = opts || {};
+	
+	var errorCodes = {
+		SUCCESS : 0,
+		MISSING_ARGUMENT : 1,
+		READ_ERROR : 2,
+		INVALID_ARGUMENT : 4,
+		NOT_OVERWRITING_ERROR : 15,
+		CANT_WRITE_ERROR : 16,
+		OUT_OF_MEMORY_ERROR : 17,
+		WRONG_ARCHITECTURE : 18, // Missing SSE
+		PNG_OUT_OF_MEMORY_ERROR : 24,
+		LIBPNG_FATAL_ERROR : 25,
+		LIBPNG_INIT_ERROR : 35,
+		TOO_LARGE_FILE : 98,
+		TOO_LOW_QUALITY : 99
+	};
 
 	return function (file, imagemin, cb) {
 		if (!isPng(file.contents)) {
@@ -51,7 +67,7 @@ module.exports = function (opts) {
 			.use(pngquant, args.concat(['-f', '-o', exec.dest(), exec.src()]))
 			.run(file.contents, function (err, buf) {
 				if (err) {
-					cb(err);
+					cb(errorCodes.TOO_LARGE_FILE ? null : err);
 					return;
 				}
 
