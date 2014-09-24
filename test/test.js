d59e039947e9431822ae776e14b69c5e5762208f
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2014-09-24 12:28:19 +0900

    Add more tests to cover various cases

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var bufferEqual = require('buffer-equal');
 var isPng = require('is-png');
 var pngquant = require('../');
 var path = require('path');
@@ -24,3 +25,36 @@ test('optimize a PNG', function (t) {
 	});
 });
 
+test('skip optimizing a non-PNG file', function (t) {
+	t.plan(2);
+
+	read(__filename, function (err, file) {
+		t.assert(!err);
+
+		var stream = pngquant();
+		var buf = file.contents;
+
+		stream.on('data', function (data) {
+			t.assert(bufferEqual(file.contents, buf));
+		});
+
+		stream.end(file);
+	});
+});
+
+test('skip optimizing an already optimized PNG', function (t) {
+	t.plan(2);
+
+	read(path.join(__dirname, 'fixtures/test-smallest.png'), function (err, file) {
+		t.assert(!err);
+
+		var stream = pngquant();
+		var buf = file.contents;
+
+		stream.on('data', function (data) {
+			t.assert(bufferEqual(file.contents, buf));
+		});
+
+		stream.end(file);
+	});
+});
