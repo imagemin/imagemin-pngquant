d7251e085e774bead8dd335c89d697df947e8b58
Kevin Martensson <kevinmartensson@gmail.com>
2014-10-25 00:45:26 +0200

    Export it as a constructor

--- index.js
+++ index.js
@@ -8,89 +8,6 @@ var through = require('through2');
 /**
  * pngquant imagemin plugin
  *
- * @param {Object} file
- * @param {String} enc
- * @param {Object} opts
- * @param {Function} cb
- * @api private
- */
-
-function plugin(file, enc, opts, cb) {
-	if (file.isNull()) {
-		cb(null, file);
-		return;
-	}
-
-	if (file.isStream()) {
-		cb(new Error('Streaming is not supported'));
-		return;
-	}
-
-	if (!isPng(file.contents)) {
-		cb(null, file);
-		return;
-	}
-
-	var args = ['-'];
-	var ret = [];
-	var len = 0;
-
-	if (opts.floyd) {
-		args.push('--floyd', opts.floyd);
-	}
-
-	if (opts.nofs) {
-		args.push('--nofs');
-	}
-
-	if (opts.posterize) {
-		args.push('--posterize', opts.posterize);
-	}
-
-	if (opts.quality) {
-		args.push('--quality', opts.quality);
-	}
-
-	if (opts.speed) {
-		args.push('--speed', opts.speed);
-	}
-
-	if (opts.verbose) {
-		args.push('--verbose');
-	}
-
-	var cp = spawn(pngquant, args);
-
-	cp.on('error', function (err) {
-		cb(err);
-		return;
-	});
-
-	cp.stderr.setEncoding('utf8');
-	cp.stderr.on('data', function (data) {
-		cb(new Error(data));
-		return;
-	});
-
-	cp.stdout.on('data', function (data) {
-		ret.push(data);
-		len += data.length;
-	});
-
-	cp.on('close', function () {
-		if (len < file.contents.length) {
-			file.contents = Buffer.concat(ret, len);
-		}
-
-		cb(null, file);
-	});
-
-	cp.stdin.end(file.contents);
-}
-
-/**
- * Module exports
- *
  * @param {Object} opts
  * @api public
  */
@@ -98,23 +15,77 @@ function plugin(file, enc, opts, cb) {
 module.exports = function (opts) {
 	opts = opts || {};
 
-	return through.obj(function (file, enc, cb) {
-		plugin(file, enc, opts, cb);
-	});
-};
-
-/**
- * Module exports constructor
- *
- * @param {Object} opts
- * @api public
- */
-
-module.exports.ctor = function (opts) {
-	opts = opts || {};
-
 	return through.ctor({ objectMode: true }, function (file, enc, cb) {
-		plugin(file, enc, opts, cb);
+		if (file.isNull()) {
+			cb(null, file);
+			return;
+		}
+
+		if (file.isStream()) {
+			cb(new Error('Streaming is not supported'));
+			return;
+		}
+
+		if (!isPng(file.contents)) {
+			cb(null, file);
+			return;
+		}
+
+		var args = ['-'];
+		var ret = [];
+		var len = 0;
+
+		if (opts.floyd) {
+			args.push('--floyd', opts.floyd);
+		}
+
+		if (opts.nofs) {
+			args.push('--nofs');
+		}
+
+		if (opts.posterize) {
+			args.push('--posterize', opts.posterize);
+		}
+
+		if (opts.quality) {
+			args.push('--quality', opts.quality);
+		}
+
+		if (opts.speed) {
+			args.push('--speed', opts.speed);
+		}
+
+		if (opts.verbose) {
+			args.push('--verbose');
+		}
+
+		var cp = spawn(pngquant, args);
+
+		cp.on('error', function (err) {
+			cb(err);
+			return;
+		});
+
+		cp.stderr.setEncoding('utf8');
+		cp.stderr.on('data', function (data) {
+			cb(new Error(data));
+			return;
+		});
+
+		cp.stdout.on('data', function (data) {
+			ret.push(data);
+			len += data.length;
+		});
+
+		cp.on('close', function () {
+			if (len < file.contents.length) {
+				file.contents = Buffer.concat(ret, len);
+			}
+
+			cb(null, file);
+		});
+
+		cp.stdin.end(file.contents);
 	});
 };
 
--- test/test.js
+++ test/test.js
@@ -13,27 +13,7 @@ test('optimize a PNG', function (t) {
 	read(path.join(__dirname, 'fixtures/test.png'), function (err, file) {
 		t.assert(!err, err);
 
-		var stream = pngquant();
-		var size = file.contents.length;
-
-		stream.on('data', function (data) {
-			t.assert(data.contents.length < size);
-			t.assert(isPng(data.contents));
-		});
-
-		stream.end(file);
-	});
-});
-
-test('optimize a PNG using ctor', function (t) {
-	t.plan(3);
-
-	var Pngquant = pngquant.ctor();
-
-	read(path.join(__dirname, 'fixtures/test.png'), function (err, file) {
-		t.assert(!err, err);
-
-		var stream = new Pngquant();
+		var stream = pngquant()();
 		var size = file.contents.length;
 
 		stream.on('data', function (data) {
@@ -51,7 +31,7 @@ test('skip optimizing a non-PNG file', function (t) {
 	read(__filename, function (err, file) {
 		t.assert(!err, err);
 
-		var stream = pngquant();
+		var stream = pngquant()();
 		var buf = file.contents;
 
 		stream.on('data', function (data) {
@@ -68,7 +48,7 @@ test('skip optimizing an already optimized PNG', function (t) {
 	read(path.join(__dirname, 'fixtures/test-smallest.png'), function (err, file) {
 		t.assert(!err, err);
 
-		var stream = pngquant();
+		var stream = pngquant()();
 		var buf = file.contents;
 
 		stream.on('data', function (data) {
