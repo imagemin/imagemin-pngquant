a42e5343b661b077f078c243bb18e14480a12705
Dan Webb <mail@danwebb.co>
2019-01-02 14:42:35 +0000

    Remove tsd-check IIFE

--- index.test-d.ts
+++ index.test-d.ts
@@ -3,12 +3,10 @@ import * as path from 'path';
 import { expectType } from 'tsd-check';
 import imageminPngquant from '.';
 
-(async () => {
-  const buffer = await fs.readFileSync(path.join(__dirname, 'fixture.png'));
+const buffer = await fs.readFileSync(path.join(__dirname, 'fixture.png'));
 
-  expectType<Buffer>(await imageminPngquant()(buffer));
-  expectType<Buffer>(await imageminPngquant({
-    speed: 10,
-    quality: 100
-  })(buffer));
-})();
+expectType<Buffer>(await imageminPngquant()(buffer));
+expectType<Buffer>(await imageminPngquant({
+  speed: 10,
+  quality: 100
+})(buffer));
