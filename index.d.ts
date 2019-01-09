fd6d844e22453ca32d96942ec4373c83f939a09e
Sindre Sorhus <sindresorhus@gmail.com>
2019-01-10 06:35:53 +0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 export interface Options {
   /**
-   * Speed 10 has 5% lower quality, but is about 8 times faster than the default. Speed 11 disables dithering and lowers compression level.
+   * Speed `10` has 5% lower quality, but is about 8 times faster than the default. Speed `11` disables dithering and lowers compression level.
    * 
    * @default 3
    */
@@ -18,24 +18,22 @@ export interface Options {
    * Min and max are numbers in range 0 (worst) to 1 (perfect), similar to JPEG.
    * 
    * @example [0.3, 0.5]
-   * 
    */
   quality?: number[];
 
   /**
    * Set the dithering level using a fractional number between 0 (none) and 1 (full).
-   * Pass in false to disable dithering.
+   * Pass in `false` to disable dithering.
    * 
    * @default 1
    */
   dithering?: number | boolean;
 
   /**
-   * Truncate number of least significant bits of color (per channel). Use this when image will be output on low-depth displays (e.g. 16-bit RGB). pngquant will make almost-opaque pixels fully opaque and will reduce amount of semi-transparent colors.
+   * Truncate number of least significant bits of color (per channel).
+	 * Use this when image will be output on low-depth displays (e.g. 16-bit RGB). pngquant will make almost-opaque pixels fully opaque and will reduce amount of semi-transparent colors.
    */
   posterize?: number;
-
-
 
   /**
    * Print verbose status messages.
@@ -53,8 +51,8 @@ export interface Options {
 export type Plugin = (input: Buffer | NodeJS.ReadableStream) => Promise<Buffer>
 
 /**
- * Imagemin plugin for pngquant
+ * Imagemin plugin for pngquant.
  * 
- * @returns An imagemin plugin.
+ * @returns An Imagemin plugin.
  */
 export default function imageminPngquant(options?: Options): Plugin;
