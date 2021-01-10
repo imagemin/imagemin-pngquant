149fecde56fa61202f9516b25ded9d780247e2ac
Ryan Lopopolo <rjl@hyperbo.la>
2021-01-10 06:51:45 -0800

    Upgrade dependencies to latest versions
    - execa@^5.0.0
    - ow@^0.22.0

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const execa = require('execa');
 const isPng = require('is-png');
 const isStream = require('is-stream');
 const pngquant = require('pngquant-bin');
-const ow = require('ow');
+const {default: ow} = require('ow');
 
 const imageminPngquant = (options = {}) => input => {
 	const isBuffer = Buffer.isBuffer(input);
